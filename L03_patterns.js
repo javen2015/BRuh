c31ff53c60c24706f32be85e772b11d99008441b
javen2015 <zqlin15@gmail.com>
2025-08-02 06:34:26 +0000

    Aug 2, 2025, 6:34 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -22,7 +22,7 @@ function draw() {
   let circleDiameter = 30;
   let numOfCircles = 5;
   let totalWidth = circleDiameter* numOfCircles
-  
+  let startX = 
   fill(0,0,0);
   for (let i = 0; i < 6; i++) {
     circle()
