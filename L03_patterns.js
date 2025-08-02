98659754c45c0bbdb9b04a9621c4c0b373d3232d
javen2015 <zqlin15@gmail.com>
2025-08-02 07:02:35 +0000

    Aug 2, 2025, 7:02 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -30,16 +30,16 @@ function draw() {
 
   for (let row = 0; row < numOfCircles; row++) {
     let rowY = startY + (circleDiameter*row);
-    
-  }
-
-  for (let i = 0; i < numOfCircles; i++) {
+    for (let i = 0; i < numOfCircles; i++) {
       fill(colourR,colourB,colourG);
-    let x = startX + i* circleDiameter
+      let x = startX + i* circleDiameter
     circle(x,height/2,circleDiameter,circleDiameter)
     colourR += 50
     colourG += 20
     colourB += 30
   }
+  }
+
+  
 }
 
