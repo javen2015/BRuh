05eb8985efa2bc423f2d0d7555015d08c83729bb
javen2015 <zqlin15@gmail.com>
2025-08-16 06:41:47 +0000

    Aug 16, 2025, 6:41 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -5,9 +5,10 @@ function setup() {
   }
 
 let shapeColor ='blue';
-let x1 = random(1000)
-let x2 = random(1000)
-let x3 = random(1000)
+let x1 = random(1000);
+let x2 = random(1000);
+let x3 = random(1000);
+let
 function draw(){
     triangle(triangleValue)
     fill(shapeColor);
