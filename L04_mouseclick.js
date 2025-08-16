d8deab01fe6165db8efb96305581201220e232ac
javen2015 <zqlin15@gmail.com>
2025-08-16 06:58:42 +0000

    Aug 16, 2025, 6:58 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -68,3 +68,30 @@ function setup() {
 // function mouseMoved(){
 //     circleSize +=2
 // }
+
+// let shapeColor ="blue";
+// let circleSize = 20
+
+
+// function setup(){
+//     createCanvas(400,400)
+//     background(200);
+// }
+
+// function draw(){
+//     fill(shapeColor);
+//     ellipse(mouseX,mouseY,circleSize,circleSize) 
+// }
+
+// function mousePressed(){
+//     shapeColor = color(random(255), random(255),random(255))
+//     circleSize= 1
+// }
+
+// function mouseReleased(){
+//     shapeColor = color(random(255), random(255),random(255))
+// }
+
+// function mouseMoved(){
+//     circleSize +=2
+// }
