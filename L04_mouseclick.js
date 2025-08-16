9e436d88e2b115215f7ef400e3b613a17751ff59
javen2015 <zqlin15@gmail.com>
2025-08-16 06:43:34 +0000

    Aug 16, 2025, 6:43 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -17,6 +17,11 @@ function draw(){
 
 function mousePressed(){
     shapeColor= color(random(255),random(255),random(255))
+    let x1 = random(1000);
+    let x2 = random(1000);
+    let x3 = random(1000);
+let width = random(1000);
+let height = random(1000);
 }
 
 function mouseReleased(){
