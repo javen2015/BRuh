88841cd7e745592427b03e206a8074a1bbf02b90
javen2015 <zqlin15@gmail.com>
2025-08-16 06:52:13 +0000

    Aug 16, 2025, 6:52 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -43,24 +43,24 @@ function draw(){
     
 }
 
-// write your codes here
 
-let shapeColor ="blue";
 
-function setup(){
-    createCanvas(400,400)
-    background(200);
-}
+// let shapeColor ="blue";
 
-function draw(){
-    fill(shapeColor);
-    ellipse(200,200,80,80) 
-}
+// function setup(){
+//     createCanvas(400,400)
+//     background(200);
+// }
 
-function mousePressed(){
-    shapeColor = color(random(255), random(255),random(255))
-}
+// function draw(){
+//     fill(shapeColor);
+//     ellipse(200,200,80,80) 
+// }
 
-function mouseReleased(){
-    shapeColor = color(random(255), random(255),random(255))
-}
+// function mousePressed(){
+//     shapeColor = color(random(255), random(255),random(255))
+// }
+
+// function mouseReleased(){
+//     shapeColor = color(random(255), random(255),random(255))
+// }
