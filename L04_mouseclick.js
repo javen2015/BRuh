6d605dbeee685aa0dff3733b6456956a19e97155
javen2015 <zqlin15@gmail.com>
2025-08-16 06:50:46 +0000

    Aug 16, 2025, 6:50 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -4,37 +4,37 @@ function setup() {
     // noLoop(); // Stops continuous drawing
   }
 
-let shapeColor ='blue';
-let x1 = 500;
-let y1 = 200;
-let x2 = 300;
-let y2 = 500;
-let x3 = 200;
-let y3 = 300;
-let height = 500;
-function draw(){
-    if (mouseIsPressed){
-        triangle(x1,y1,x2,y2,x3,y3);
-        fill(shapeColor);
-    }
-}
+// let shapeColor ='blue';
+// let x1 = 500;
+// let y1 = 200;
+// let x2 = 300;
+// let y2 = 500;
+// let x3 = 200;
+// let y3 = 300;
+// let height = 500;
+// function draw(){
+//     if (mouseIsPressed){
+//         triangle(x1,y1,x2,y2,x3,y3);
+//         fill(shapeColor);
+//     }
+// }
 
-function mousePressed(){
-    shapeColor= color(random(255),random(255),random(255))
-    let x1 = random(width);
-    let y1 = random(height);
-    let x2 = random(width);
-    let y2 = random(width);
-    let x3 = random(height);
-    let y3 = random(width);
-}
+// function mousePressed(){
+//     shapeColor= color(random(255),random(255),random(255))
+//     let x1 = random(width);
+//     let y1 = random(height);
+//     let x2 = random(width);
+//     let y2 = random(width);
+//     let x3 = random(height);
+//     let y3 = random(width);
+// }
 
-function mouseReleased(){
-    shapeColor =color(random(255),random(255),random(255))
-    let x1 = random(width);
-    let y1 = random(height);
-    let x2 = random(width);
-    let y2 = random(width);
-    let x3 = random(height);
-    let y3 = random(width);
-}
+// function mouseReleased(){
+//     shapeColor =color(random(255),random(255),random(255))
+//     let x1 = random(width);
+//     let y1 = random(height);
+//     let x2 = random(width);
+//     let y2 = random(width);
+//     let x3 = random(height);
+//     let y3 = random(width);
+// }
