1ed43baa32be1f78a949c7539038f30a58e0c22c
javen2015 <zqlin15@gmail.com>
2025-08-23 06:15:07 +0000

    Aug 23, 2025, 6:15 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -120,27 +120,27 @@
 //     ellipse(mouseX,mouseY,circleSize,circleSize);
 // }
 
+// Supe
+// function setup() {
+//     createCanvas(1000, 500);
+//     background(220);
+//     // noLoop(); // Stops continuous drawing
+//   }
 
-function setup() {
-    createCanvas(1000, 500);
-    background(220);
-    // noLoop(); // Stops continuous drawing
-  }
 
+// function mousePressed(){
 
-function mousePressed(){
+//     shapeColor = color(random(255), random(255),random(255))
+//     fill(shapeColor);
+//     circle(mouseX,mouseY,20)
+// }
 
-    shapeColor = color(random(255), random(255),random(255))
-    fill(shapeColor);
-    circle(mouseX,mouseY,20)
-}
+// function mouseDragged(){
 
-function mouseDragged(){
-
-    shapeColor = color(random(255), random(255),random(255))
-    fill(shapeColor);
-    circle(mouseX,mouseY,20)
-}
+//     shapeColor = color(random(255), random(255),random(255))
+//     fill(shapeColor);
+//     circle(mouseX,mouseY,20)
+// }
 
 
 
