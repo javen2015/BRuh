687b7344b7ddcdd537ebbd17abd64e651620a3d8
javen2015 <zqlin15@gmail.com>
2025-08-23 07:30:41 +0000

    Aug 23, 2025, 7:30 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,11 +1,11 @@
 let rectangleSize = 50;
 function setup() {
     createCanvas(1000, 500);
-    background(220);
   }
 
 
 function draw(){
+    background(220);
     rect(width/2 - rectangleSize/2 , height/2 - rectangleSize/2, rectangleSize, rectangleSize);
 }
 
