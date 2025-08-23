d05db6a0cb786a1b60c9aafefaaf21440d592b5b
javen2015 <zqlin15@gmail.com>
2025-08-23 06:14:23 +0000

    Aug 23, 2025, 6:14 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -129,11 +129,15 @@ function setup() {
 
 
 function mousePressed(){
+
+    shapeColor = color(random(255), random(255),random(255))
     fill(shapeColor);
     circle(mouseX,mouseY,20)
 }
 
 function mouseDragged(){
+
+//     shapeColor = color(random(255), random(255),random(255))
     fill(shapeColor);
     circle(mouseX,mouseY,20)
 }
