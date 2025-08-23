a1125a860dfe044f0df8de515577bf7ecc2ff477
javen2015 <zqlin15@gmail.com>
2025-08-23 07:08:26 +0000

    Aug 23, 2025, 7:08 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,26 +1,4 @@
-function setup() {
-    createCanvas(400, 600);
-    background(220);
-    noStroke(); // no outline
-  }
-
-let size = 5;
-function draw(){
-
-}
-
-function mousePressed(){
-
-    shapeColor = color(random(255), random(255),random(255))
-    size = 5;
-
-}
-
-function mouseDragged(){
-    fill(shapeColor);
-    circle(mouseX,mouseY,size)
-    size += 0.3;
-}
+// 
 // a = 400,0
 //c = 400,600
 //b = 0,600
