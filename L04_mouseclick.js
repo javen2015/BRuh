c640e3da87fc3f90370ba6b3f2e6919ecb5ed1b7
javen2015 <zqlin15@gmail.com>
2025-08-23 07:08:41 +0000

    Aug 23, 2025, 7:08 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -143,5 +143,27 @@ function mouseDragged(){
 }
 
 
+// function setup() {
+//     createCanvas(400, 600);
+//     background(220);
+//     noStroke(); // no outline
+//   }
 
+// let size = 5;
+// function draw(){
+
+// }
+
+// function mousePressed(){
+
+//     shapeColor = color(random(255), random(255),random(255))
+//     size = 5;
+
+// }
+
+// function mouseDragged(){
+//     fill(shapeColor);
+//     circle(mouseX,mouseY,size)
+//     size += 0.3;
+// }
 
