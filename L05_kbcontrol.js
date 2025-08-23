f0237b000094be43e021a22d6a7b97b50475ef61
javen2015 <zqlin15@gmail.com>
2025-08-23 07:26:40 +0000

    Aug 23, 2025, 7:26 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,19 +1,16 @@
 function setup() {
     createCanvas(1000, 500);
     background(220);
-    noStroke(); // no outline
   }
 
-let size = 5;
+
 function draw(){
 
 }
 
 function keyPressed(){
-
 }
 
 function keyReleased(){
-
 }
 
