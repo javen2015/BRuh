8b16e5b73ae705f1ca8623c7e3a382fa4c4e54d0
javen2015 <zqlin15@gmail.com>
2025-09-06 06:33:27 +0000

    Sep 6, 2025, 6:33 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,3 +1,36 @@
+// let rectangleSize = 50;
+// let showCircle = false;
+
+// function setup() {
+//     createCanvas(600, 400);
+//         background(220);
+// }
+// function draw(){
+//     background(220);
+//     if(showCircle){
+//         circle(width/2, height/2, 100);
+//     noStroke();
+//   }
+// }
+// function keyPressed(){
+//     if (key === 'c') {
+//         showCircle = !showCircle;
+//     }
+// }
+// function draw(){
+//     background(220);
+//     rect(width/2 - rectangleSize/2 , height/2 - rectangleSize/2, rectangleSize, rectangleSize);
+// }
+
+// function keyPressed(){
+//     rectangleSize = 100;
+// }
+
+// function keyReleased(){
+//     rectangleSize = 50;
+// }
+
+// console.log("Hello World")
 let rectangleSize = 50;
 let showCircle = false;
 
@@ -17,18 +50,3 @@ function keyPressed(){
         showCircle = !showCircle;
     }
 }
-// function draw(){
-//     background(220);
-//     rect(width/2 - rectangleSize/2 , height/2 - rectangleSize/2, rectangleSize, rectangleSize);
-// }
-
-// function keyPressed(){
-//     rectangleSize = 100;
-// }
-
-// function keyReleased(){
-//     rectangleSize = 50;
-// }
-
-// console.log("Hello World")
-
