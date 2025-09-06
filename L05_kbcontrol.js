c6be54368fc0b6f0469afdeeb48f259847466d49
javen2015 <zqlin15@gmail.com>
2025-09-06 07:00:14 +0000

    Sep 6, 2025, 7:00 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -31,21 +31,21 @@
 // }
 
 // console.log("Hello World")
-let rectangleSize = 50;
-let showCircle = false;
+// let rectangleSize = 50;
+// let showCircle = false;
 
-function setup() {
-    createCanvas(600, 400);
-}
-function draw(){
-    background(220);
-    if (keyCode === 67) {
-        circle(width/2, height/2, 100);
-    }
-    if (key === 's') {
-        rect(width/2,height/2,100,100);
-    }
-    if (keyCode === UP_ARROW) {
-        triangle(300,150,250,250,350,250);
-    }
-}
+// function setup() {
+//     createCanvas(600, 400);
+// }
+// function draw(){
+//     background(220);
+//     if (keyCode === 67) {
+//         circle(width/2, height/2, 100);
+//     }
+//     if (key === 's') {
+//         rect(width/2,height/2,100,100);
+//     }
+//     if (keyCode === UP_ARROW) {
+//         triangle(300,150,250,250,350,250);
+//     }
+// }
