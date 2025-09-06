5ffaa242b3b56d689c2571aafe261acc9cffc67f
javen2015 <zqlin15@gmail.com>
2025-09-06 07:10:33 +0000

    Sep 6, 2025, 7:10 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -53,3 +53,16 @@
 //     console.log("key:",key);
 //     console.log("keyCode:",keyCode);
 // }
+let colourR = 0;
+let colourG = 0;
+let colourB = 0;
+function setup() {
+    createCanvas(600, 400);
+}
+function draw(){
+    background(220);
+    circle(width/2, height/2, 100);
+    if (keyCode === UP_ARROW) {
+        coloru
+    }
+}
