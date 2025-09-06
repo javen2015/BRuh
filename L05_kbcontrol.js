00163e561eebc9b0ccb410a3cb3d7033aff9c412
javen2015 <zqlin15@gmail.com>
2025-09-06 06:54:59 +0000

    Sep 6, 2025, 6:54 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -39,7 +39,7 @@ function setup() {
 }
 function draw(){
     background(220);
-    if (keyCode === 108) {
+    if (keyCode === '') {
         circle(width/2, height/2, 100);
     }
     if (key === 'S') {
