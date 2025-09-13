fc7ee97cab4154c51992a892bb334ff5b41c6b76
javen2015 <zqlin15@gmail.com>
2025-09-13 06:40:03 +0000

    Sep 13, 2025, 6:40 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -113,30 +113,30 @@
 //         colourB = 255;
 //     }
 // }
-let x =0;
-let y = 0;
-let speed = 1;
-function setup() {
-    createCanvas(400, 400);
-}
-function draw(){
-    background(220);
-    if(keyIsDown(RIGHT_ARROW)){
-        x+=speed
-    }
-    if(keyIsDown(LEFT_ARROW)){
-        x-=speed
-    }
-    if(keyIsDown(UP_ARROW)){
-        y-=speed
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        y+=speed
-    }
-    if(keyIsDown(SHIFT)){
-        speed+=1
-    }
-    x = constrain(x,0,width)
-    y = constrain(y,0,height)
-    circle(x, y, 50);
-}
+// let x =0;
+// let y = 0;
+// let speed = 1;
+// function setup() {
+//     createCanvas(400, 400);
+// }
+// function draw(){
+//     background(220);
+//     if(keyIsDown(RIGHT_ARROW)){
+//         x+=speed
+//     }
+//     if(keyIsDown(LEFT_ARROW)){
+//         x-=speed
+//     }
+//     if(keyIsDown(UP_ARROW)){
+//         y-=speed
+//     }
+//     if(keyIsDown(DOWN_ARROW)){
+//         y+=speed
+//     }
+//     if(keyIsDown(SHIFT)){
+//         speed+=1
+//     }
+//     x = constrain(x,0,width)
+//     y = constrain(y,0,height)
+//     circle(x, y, 50);
+// }
