520a44968913924ceeb0fd2ed49bbfd36690fd9e
javen2015 <zqlin15@gmail.com>
2025-09-13 07:10:13 +0000

    Sep 13, 2025, 7:10 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,33 +1,33 @@
-let x =200;
-let y = 200;
-let colour = 1;
-function setup() {
-    createCanvas(400, 400);
-}
-function draw(){
-    background(220);
-    if(keyIsDown(RIGHT_ARROW)){
-        x+=10;
-    }
-    if(keyIsDown(LEFT_ARROW)){
-        x-=10;
-    }
-    if(keyIsDown(UP_ARROW)){
-        y-=10;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        y+=10;
-    }
-    if(keyIsDown(SPACE)){
-        if(colour%2==0){
-            colour+=1;
-            fill(50,0,0);
-        }else{
-            colour+=1;
-            fill(200,0,0);
-        }
-    }
-    x = constrain(x,50,width-50);
-    y = constrain(y,50,height-50);
-    square(x, y, 50,50);
-}
+// let x =200;
+// let y = 200;
+// let colour = 1;
+// function setup() {
+//     createCanvas(400, 400);
+// }
+// function draw(){
+//     background(220);
+//     if(keyIsDown(RIGHT_ARROW)){
+//         x+=10;
+//     }
+//     if(keyIsDown(LEFT_ARROW)){
+//         x-=10;
+//     }
+//     if(keyIsDown(UP_ARROW)){
+//         y-=10;
+//     }
+//     if(keyIsDown(DOWN_ARROW)){
+//         y+=10;
+//     }
+//     if(keyIsDown(SPACE)){
+//         if(colour%2==0){
+//             colour+=1;
+//             fill(50,0,0);
+//         }else{
+//             colour+=1;
+//             fill(200,0,0);
+//         }
+//     }
+//     x = constrain(x,50,width-50);
+//     y = constrain(y,50,height-50);
+//     square(x, y, 50,50);
+// }
