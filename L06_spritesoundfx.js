27ff79394719b53207653975e7016332b0ef9fde
javen2015 <zqlin15@gmail.com>
2025-09-20 06:40:00 +0000

    Sep 20, 2025, 6:40 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -52,7 +52,7 @@ function draw(){
     if(keyIsDown(DOWN_ARROW)){
         y+=10;
     }
-     x = constrain(x,50,width-50);
+     x = constrain(x,50,200);
      y = constrain(y,50,height-50);
 }
 function setup() {
