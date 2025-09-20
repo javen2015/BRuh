c32d809d460211149c753fbca914c2ddaf2b6907
javen2015 <zqlin15@gmail.com>
2025-09-20 07:18:45 +0000

    Sep 20, 2025, 7:18 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -18,7 +18,7 @@ function draw(){
     if(keyIsDown(DOWN_ARROW)){
         y+=10;
     }
-    x = constrain(x,50,width-50);
-    y = constrain(y,50,height-50);
+    x = constrain(x,0,width);
+    y = constrain(y,0,height);
     circle(x, y,50);
 }
