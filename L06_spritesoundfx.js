36c123269616cdd6363587266835a73165467016
javen2015 <zqlin15@gmail.com>
2025-09-20 06:41:41 +0000

    Sep 20, 2025, 6:41 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -58,7 +58,7 @@ function draw(){
 function setup() {
     createCanvas(200,200);
     background('lightblue');
-    image(staticImage,50,50,100,100);
+    image(staticImage,0,0,100,100);
     backgroundSound.loop();
 }
 function keyPressed(){
