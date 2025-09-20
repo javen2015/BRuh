0575a7c55f8ea7dbc39e2a6c7d834735885f72ff
javen2015 <zqlin15@gmail.com>
2025-09-20 06:09:54 +0000

    Sep 20, 2025, 6:09 AM

--- midsem.js
+++ midsem.js
@@ -7,7 +7,7 @@ function setup() {
 function draw() {
     stroke("red")
     strokeWeight(10);
-    fill
+    fill("black")
     circle(150,200,100);
     triangle(250,250,300,150,350,250);
     rect(400,150,100,100);
