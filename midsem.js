c19b5cb51b76125f6ab6781bb8eb87c58d0cddc4
javen2015 <zqlin15@gmail.com>
2025-09-20 06:10:46 +0000

    Sep 20, 2025, 6:10 AM

--- midsem.js
+++ midsem.js
@@ -1,14 +1,14 @@
-function setup() {
-  createCanvas(600,400);
-  background(0);
+// function setup() {
+//   createCanvas(600,400);
+//   background(0);
   
-}
+// }
 
-function draw() {
-    stroke("red")
-    strokeWeight(10);
-    fill("black")
-    circle(150,200,100);
-    triangle(250,250,300,150,350,250);
-    rect(400,150,100,100);
-}
+// function draw() {
+//     stroke("red")
+//     strokeWeight(10);
+//     fill("black")
+//     circle(150,200,100);
+//     triangle(250,250,300,150,350,250);
+//     rect(400,150,100,100);
+// }
