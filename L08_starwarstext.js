09c7a0bc0bdf4b522d78b293473238e15047eecf
javen2015 <zqlin15@gmail.com>
2025-09-27 07:12:51 +0000

    Sep 27, 2025, 7:12 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -2,7 +2,7 @@ function setup(){
     createCanvas(400,400);
     background(220);
     textSize(30);
-    text("Javen",0 , 150);
+    text("Javen",50 , 50);
     text("10",50,100);
     text("Programming",50,150);
 }
