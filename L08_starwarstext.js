ddb2753544d63b4c47583245fa8488bb4aa56a3b
javen2015 <zqlin15@gmail.com>
2025-09-27 07:28:05 +0000

    Sep 27, 2025, 7:28 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -4,7 +4,7 @@ function setup(){
     createCanvas(400,400);
     background(220);
     textSize(30);
-    text("Javen",50 , 50);
-    text("10",50,100);
-    text("Programming",50,150);
+    for (let i = 0; i < favouriteFoods.length; i++){
+    text((i+1)+ ". " + favouriteFoods[i], 50, 80 + i*30);
+}
 }
