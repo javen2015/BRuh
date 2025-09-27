9099019ec1f1d8fbce0404c7c1811fe703eb1134
javen2015 <zqlin15@gmail.com>
2025-09-27 06:53:31 +0000

    Sep 27, 2025, 6:53 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -4,11 +4,12 @@ function setup(){
 }
 let x = 0;
 let y = 200;
+let direction = 5;
 function draw(){    
     square(x,y,50);
-    x+=5;
+    x+=direction;
     if (x - 50 <= 0 || x + 5 >= width) {
         fill(random(255),random(255),random(255));
-        ball *= -1;
+        direction = 5
     }
 }
