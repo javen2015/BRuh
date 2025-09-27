238fe74267695d046a4caee8b14d551bd07790b5
javen2015 <zqlin15@gmail.com>
2025-09-27 06:58:10 +0000

    Sep 27, 2025, 6:58 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -3,12 +3,11 @@ function setup(){
 }
 let x = 100;
 let y = 200;
-let speedX = 1;
-let direction = 5;
+let speedX = 5;
 function draw(){    
     background(220);
     x+=speedX;
-    if (x - 50/2 <= 0 || x + 50/2 >= width) {
+    if (x - 50 <= 0 || x + 50 >= width) {
         fill(random(255),random(255),random(255));
         speedX *= -1;
     }
