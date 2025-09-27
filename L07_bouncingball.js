881a350e6e90eae43d11f086c3156cafecbecd75
javen2015 <zqlin15@gmail.com>
2025-09-27 06:17:40 +0000

    Sep 27, 2025, 6:17 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -7,7 +7,7 @@ function setup() {
     createCanvas(400, 400);
 }
 function draw(){
-    background(220);
+    // background(220);
     // if(keyIsDown(RIGHT_ARROW)){
     //     ballX+=ballSpeedX;
     // }
