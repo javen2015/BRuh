f01f24a5a636813f893970e25e1645d4b34f09c9
javen2015 <zqlin15@gmail.com>
2025-09-27 06:17:14 +0000

    Sep 27, 2025, 6:17 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -26,7 +26,7 @@ function draw(){
     ballX += ballSpeedX;
     ballY += ballSpeedY;
     if (ballX - ballSize/2 <= 0 || ballX + ballSize/2 >= width) {
-        fill(random())
+        fill(random(255),random(255),random(255));
         ballSpeedX *= -1;
     }
     if(ballY - ballSize/2 <= 0 || ballY + ballSize/2 >= height){
