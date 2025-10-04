190de70d83f5a96b6cfc1905106c92a5e10f3c1b
javen2015 <zqlin15@gmail.com>
2025-10-04 06:21:35 +0000

    Oct 4, 2025, 6:21 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,3 +1,17 @@
+// let favouriteFoods = ["Pizza","Sushi","Ice Cream","Hotpot","Pasta"];
+
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     textSize(40);
+//     textAlign(CENTER+200,CENTER+400);
+//     text("favourite foods",100,30);
+// }
+// function draw(){
+//     for (let i = 0; i < favouriteFoods.length; i++){
+//         text((i+1)+ ". " + favouriteFoods[i], 50, 80 + i*30);
+// }
+// }
 let favouriteFoods = ["Pizza","Sushi","Ice Cream","Hotpot","Pasta"];
 
 function setup(){
