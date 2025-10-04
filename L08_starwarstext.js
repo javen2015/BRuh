cb00b171ef82eb6ca306fdce01c946f7729fb033
javen2015 <zqlin15@gmail.com>
2025-10-04 06:08:05 +0000

    Oct 4, 2025, 6:08 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -4,6 +4,7 @@ function setup(){
     createCanvas(400,400);
     background(220);
     textSize(30);
+    textAlign(screenLeft,TOP)
 }
 function draw(){
     for (let i = 0; i < favouriteFoods.length; i++){
