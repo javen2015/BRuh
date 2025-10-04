aa9abd5fdd3d387efdb61ce9269e35f9f2884815
javen2015 <zqlin15@gmail.com>
2025-10-04 07:10:39 +0000

    Oct 4, 2025, 7:10 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -92,34 +92,34 @@
 //     }
 // }
 
-function setup() {
-    createCanvas(600, 400);
-    yPos = height;
-}
+// function setup() {
+//     createCanvas(600, 400);
+//     yPos = height;
+// }
 
-function draw() {
-  background('black');  // now works as expected
-  fill(255, 255, 0);
-  textSize(24);
-  textAlign(CENTER, CENTER);
+// function draw() {
+//   background('black');  // now works as expected
+//   fill(255, 255, 0);
+//   textSize(24);
+//   textAlign(CENTER, CENTER);
 
-  if(audioStarted){
-    push();
-    translate(width / 2, yPos);
-    scale(1,3);
+//   if(audioStarted){
+//     push();
+//     translate(width / 2, yPos);
+//     scale(1,3);
 
-    for (let i = 0; i < storyText.length; i++) {
-      let sentence = storyText[i];
-      text(sentence, width / 2, i * 20);
-    }
+//     for (let i = 0; i < storyText.length; i++) {
+//       let sentence = storyText[i];
+//       text(sentence, width / 2, i * 20);
+//     }
 
-    pop();
-    yPos -= 0.6;
+//     pop();
+//     yPos -= 0.6;
 
-    if (yPos < - storyText.length * 60) {
-        yPos = height;
-    }
-  } else {
-    text("Click to Start", width/2, height/2);
-  }
-}
+//     if (yPos < - storyText.length * 60) {
+//         yPos = height;
+//     }
+//   } else {
+//     text("Click to Start", width/2, height/2);
+//   }
+// }
