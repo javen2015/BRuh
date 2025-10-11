136c28058b6f2706bfcaebd3a48f4f200341daa6
javen2015 <zqlin15@gmail.com>
2025-10-11 06:39:02 +0000

    Oct 11, 2025, 6:39 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -14,7 +14,10 @@ function setup(){
 
 function draw(){
     background(220);
-    timer = 5
     textSize(100);
-    text(h+ ':' + m+ ':' + s,width/2,height/2);
+    text(timer,width/2,height/2);
+}
+function mousePressed(){
+    timer = 5;
+    setInterval(timeIt,1000);
 }
