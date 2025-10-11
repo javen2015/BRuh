83dce1376254fdab681208b05fcc8a5f02de2906
javen2015 <zqlin15@gmail.com>
2025-10-11 06:47:29 +0000

    Oct 11, 2025, 6:47 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -17,7 +17,7 @@ function draw(){
     background(220);
     textSize(100);
     text(countdown,width/2,height/2);
-    text("Click to Start",width/2,height - 50);
+    text("Click me to start the timer",width/2,height - 50);
 }
 function mousePressed(){
     interval = setInterval(updateCountdown,1000);
