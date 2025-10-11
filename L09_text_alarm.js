e0f769332ca0779680b115c9b4ec53f776650d72
javen2015 <zqlin15@gmail.com>
2025-10-11 06:37:34 +0000

    Oct 11, 2025, 6:37 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -3,6 +3,15 @@ function setup(){
     textAlign(CENTER, CENTER);
 }
 
+// function draw(){
+//     background(220);
+//     h = nf(hour(),2);
+//     m = nf(minute(),2);
+//     s= nf(second(),2);
+//     textSize(100);
+//     text(h+ ':' + m+ ':' + s,width/2,height/2);
+// }
+
 function draw(){
     background(220);
     h = nf(hour(),2);
