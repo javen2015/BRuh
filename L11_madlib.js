a103091b45b549292588d50add193fcb9e27f2b5
javen2015 <zqlin15@gmail.com>
2025-11-01 07:10:54 +0000

    Nov 1, 2025, 7:10 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -7,6 +7,8 @@ function setup(){
     background(220);
     inputField = createInput("Enter text here ");
     inputField.position(55, 20);
+    inputField = createInput("Enter text here ");
+    inputField.position(55, 20);
     submitButton = createButton('show');
     submitButton.position(inputField.x + inputField.width + 10, inputField.y);
     submitButton.mousePressed(displayInput);
@@ -15,7 +17,7 @@ function setup(){
 function displayInput(){
     background(220);
     const inputValue = inputField.value();
-    textSize(64);
+    textSize(24);
     textAlign(CENTER, CENTER);
     text(inputValue, 200,100);
 }
