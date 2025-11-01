5c7b4b745beea46ca5df8fc0d9d428ac88a84e4f
javen2015 <zqlin15@gmail.com>
2025-11-01 06:29:26 +0000

    Nov 1, 2025, 6:29 AM

--- L10_name.js
+++ L10_name.js
@@ -48,11 +48,11 @@
 // let inputText;
 // let userText = "Your Text Here";
 // function setup(){
-//     createCanvas(600,400);
-//     inputText = createInput();
-//     inputText.position(200, height -80);
-//     inputText.size(200);
-//     inputText.input(updateText);
+    // createCanvas(600,400);
+    // inputText = createInput();
+    // inputText.position(200, height -80);
+    // inputText.size(200);
+    // inputText.input(updateText);
 // }
 // function draw(){
 //     background(220);
