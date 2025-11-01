18f0a4f07d863bc0549d7768bfe6df11ab7688af
javen2015 <zqlin15@gmail.com>
2025-11-01 06:44:20 +0000

    Nov 1, 2025, 6:44 AM

--- L10_name.js
+++ L10_name.js
@@ -89,7 +89,7 @@ function draw(){
     textSize(16);
     textAlign(LEFT);
     text("Your Name:",20,height +115);
-    text("Your Age:",20,height +85);
+    text("Your Age:",20,height);
     fill(0);
     text('Pick a background color',20,50);
     fill(255);
