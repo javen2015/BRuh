e8c62ac837969c0bfb35ebf587f3841e479e476c
javen2015 <zqlin15@gmail.com>
2025-11-01 07:11:17 +0000

    Nov 1, 2025, 7:11 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -8,7 +8,8 @@ function setup(){
     inputField = createInput("Enter text here ");
     inputField.position(55, 20);
     inputField = createInput("Enter text here ");
-    inputField.position(55, 40);
+    inputField.position(55, 45);
+    
     submitButton = createButton('show');
     submitButton.position(inputField.x + inputField.width + 10, inputField.y);
     submitButton.mousePressed(displayInput);
