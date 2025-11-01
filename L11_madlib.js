fbf71f43cc133d64b75e6a7ef5f5196f1e46c903
javen2015 <zqlin15@gmail.com>
2025-11-01 07:06:34 +0000

    Nov 1, 2025, 7:06 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,3 +1,14 @@
 let inputField;
 let submitButton;
-let outputArea
+let outputArea;
+
+function setup(){
+    createCanvas(600,400);
+    background(220);
+    inputField = createInput("Enter text here ");
+    inputField.position(55, 20);
+    submitButton = createButton('show');
+    submitButton.position(inputField.x + inputField.width + 10, inputField.y);
+    submitButton.mousePressed(displayInput);
+}
+
