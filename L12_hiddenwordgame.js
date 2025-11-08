b97d9a4c55cc5b8c8340b9ec3d33de6f0a4a14ce
javen2015 <zqlin15@gmail.com>
2025-11-08 07:10:52 +0000

    Nov 8, 2025, 7:10 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -6,5 +6,10 @@ function setup() {
     background(220);
     inputField = createInput(" ");
     inputField.position(300, 300);
-    
+    submitButton = createButton("show");
+    submitButton.position(width/2,height/2);
+    submitButton.mousePressed(displayInput);
+}
+function displayInput(){
+    Text(inputField.value(),50,100);
 }
