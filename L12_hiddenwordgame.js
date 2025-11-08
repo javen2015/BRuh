49f3779a59a9b0fa87401882232f51ca6d899415
javen2015 <zqlin15@gmail.com>
2025-11-08 07:05:08 +0000

    Nov 8, 2025, 7:05 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,1 +1,8 @@
-let inputField
+let inputField;
+
+function setup() {
+    createCanvas(400, 200);
+    background(220);
+    inputField = createInput("Enter a hidden word");
+    inputField.position(50, 50);
+}
