86e475b731a6063d54ed8db9e0f5658fd95bab2a
javen2015 <zqlin15@gmail.com>
2025-11-10 10:22:23 +0000

    Nov 10, 2025, 10:22 AM

--- L13_wordscramblegame.js
+++ L13_wordscramblegame.js
@@ -42,7 +42,7 @@ function checkAnswer() {
     }else{
         for (let i = 0; i < userinput.length; i++) {
             if (userinput[i] === selected[i]) {
-                hints += " " + userinput[i];
+                
             }
         }
         message = "Wrong! Try again.";
